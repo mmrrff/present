a533defdac9588e571dbe529332939a92a708261
mmrrff <166844037+mmrrff@users.noreply.github.com>
2024-04-14 17:33:09 +0200

    Delete heart code directory

--- heart code/heart.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let heart = document.getElementById("heart");
-
-    heart.onclick = () => {
-        heart.style.color = "#cc190d";
-        heart.textContent = "I Love You ❤️Jana❤️";
-    }
-});
